1ff542fabfc7654110cf1bd5233351bae32d17ad
Eko Julianto Salim <ekojsalim@gmail.com>
2017-08-16 04:31:27 +0700

    fix >5

--- index.js
+++ index.js
@@ -109,7 +109,8 @@ function load() {
       }
     };
     agendaString = '==AGENDA=='.concat("\n");
-    task.forEach((a)=> {
+    task5 = task.slice(4);
+    task5.forEach((a)=> {
       let lessonValid = imgUrl[a.lesson.toLowerCase()];
       let url = lessonValid ? lessonValid : "https://cdn.pixabay.com/photo/2015/03/10/22/47/pc-667863_640.png";
       let tempObj = {
