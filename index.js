ee170d2e8b7c5f54ceafd8d82e44af0bbcaff771
Eko Julianto Salim <ekojsalim@gmail.com>
2017-08-15 16:50:19 +0700

    Img3

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ function load() {
       }
     };
     agendaString = '==AGENDA=='.concat("\n");
-
+    console.log(task);
     task.forEach((a)=> {
       let lessonValid = imgUrl[a.lesson.toLowerCase()];
       let url = lessonValid ? lessonValid : "https://cdn.pixabay.com/photo/2015/03/10/22/47/pc-667863_640.png";
